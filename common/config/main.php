f9b975e99361e15d5f62163d9928f09c16e3500a
support <support@eww12.eww.local>
2015-07-29 11:08:57 +0530

    divya commit

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendorsdfsdfsd',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
