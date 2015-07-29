8427e2aff0ac337a9c285dd22c2b986e3dac56d9
manasa <manasa@EWW5.eww.local>
2015-07-29 12:58:09 +0530

    changes

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendorfdsdfs',
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
