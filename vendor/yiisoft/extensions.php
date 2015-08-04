1caa2198e032e44c5e94c2ff1cac10b52cf1d057
santosh <santosh@EWW18.eww.local>
2015-08-04 14:51:41 +0530

    grocery

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -57,4 +57,13 @@ return array (
       '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
     ),
   ),
+  'yiisoft/yii2-authclient' => 
+  array (
+    'name' => 'yiisoft/yii2-authclient',
+    'version' => '2.0.4.0',
+    'alias' => 
+    array (
+      '@yii/authclient' => $vendorDir . '/yiisoft/yii2-authclient',
+    ),
+  ),
 );
