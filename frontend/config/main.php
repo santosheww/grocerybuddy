c87c6587a36280f8e70f6d8fe252c98c03fa1c28
ewwgit <support@expertwebworx.com>
2015-08-04 10:31:07 +0530

    test success

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -21,7 +21,7 @@ $config = [
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning test'],
+                    'levels' => ['error', 'warning'],
                 ],
             ],
         ],
