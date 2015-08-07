0a684b62180b0bb92e5a422b612afed574680d7b
manasa <manasa@EWW5.eww.local>
2015-08-07 09:03:55 +0530

    backup

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2database',
+            'dsn' => 'mysql:host=localhost;dbname=grocery',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
