6dcea388b87bc009df5b40f962cb020a99976df2
manasa <manasa@EWW5.eww.local>
2015-08-07 11:18:08 +0530

    register-siri

--- frontend/controllers/RegisterController.php
+++ frontend/controllers/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller {
 			
 			
 		} else {
-			return $this->render('registersrisdf',[
+			return $this->render('register',[
 					'model'=>$model,
 	
 			]);
