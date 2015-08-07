f741fc2b0eaa1fa7ee5b5c2323cadcb64516c619
manasa <manasa@EWW5.eww.local>
2015-08-07 09:03:55 +0530

    backup

--- frontend/config/params-local.php
+++ frontend/config/params-local.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.1.*;dbname=grocery_db',
+    'dsn' => 'mysql:host=localhost;dbname=grocery',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
