0c0a0928e9822ae0c21163dfc6a6869f62929563
santosh <santosh@EWW18.eww.local>
2015-08-13 11:18:44 +0530

    template intigration

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -9,68 +9,220 @@ use frontend\widgets\Alert;
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-AppAsset::register($this);
+AppAsset::register ( $this );
 ?>
-<?php $this->beginPage() ?>
+<?php $this->beginPage()?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
+<meta charset="<?= Yii::$app->charset ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags()?>
     <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+    <?php $this->head()?>
 </head>
 <body>
-    <?php $this->beginBody() ?>
-    <div class="wrap">
-        <?php
-            NavBar::begin([
-                'brandLabel' => 'My Company',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            $menuItems = [
-                ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
-            ];
-            if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Register', 'url' => ['/register/register']];
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-            } else {
-                $menuItems[] = [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ];
-            }
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => $menuItems,
-            ]);
-            NavBar::end();
-        ?>
+    <?php $this->beginBody()?>
+    <div class="top_bg">
+    <div class="container">
+		<div class="row">
+			<div class="top_right">
+				<?php echo DatePicker::widget([
+    'name' => 'check_issue_date', 
+    'value' => date('d-M-Y', strtotime('+2 days')),
+    'options' => ['placeholder' => 'Select issue date ...'],
+    'pluginOptions' => [
+        'format' => 'dd-M-yyyy',
+        'todayHighlight' => true
+    ]
+]); ?>
+			</div>
+			<div class="top_left">
+      <?php
+      if (Yii::$app->user->isGuest) {
+      	$menuItems [] = [
+      			'label' => 'Sign Up',
+      			'url' => ['/register/register']
+      			];
+      	$menuItems [] = [
+      			'label' => 'Sign In',
+      			'url' => ['/site/login']
+      			];
+      	$menuItems [] = [
+      			'label' => 'Download App',
+      			'url' => ['/site/#']
+      			];
+      	$menuItems [] = [
+      			'label' => 'Contac Us',
+      			'url' => ['/site/contact']
+      			];
+      	} 
+      	else
+      	{
+      		$menuItems [] = [
+      				'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+      				'url' => ['/site/logout'],
+      				'linkOptions' => ['data-method' => 'post']
+      				];
+      		}
+      		echo Nav::widget ([
+      				'options' => ['class' => 'navbar-nav navbar-right'],
+      				'items' => $menuItems 
+						]);
+      		?>
+      </div>
 
-        <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+		</div>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<div class="header_top">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="logo">
+						<a href="#"><img src="images/logo.png"></a>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="search">
+						<form>
+							<input type="text" value="" placeholder="Search for sale item"> <input
+								type="submit" value="">
+						</form>
+					</div>
+				</div>
+				<div class="col-sm-3">
+					<div class="right-side">
+						<div class="rewards">
+							<a href="#"><img src="images/rewards.png">Earn more Rewards</a>
+						</div>
+						<div class="store">
+							<a href="#"><img src="images/store.png">Do you own Store?</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php
+NavBar::begin ( [ ]
+// 'brandLabel' => Html::img ( 'images/logo.png' ),
+// 'brandUrl' => Yii::$app->homeUrl,
+// 'options' => [
+// 'class' => ''
+// ]
+
+ );
+$menuItems = [ 
+		[ 
+				'label' => 'Home',
+				'url' => [ 
+						'/site/index' 
+				] 
+		],
+		[ 
+				'label' => 'About',
+				'url' => [ 
+						'/site/about' 
+				] 
+		],
+		[ 
+				'label' => 'Contact',
+				'url' => [ 
+						'/site/contact' 
+				] 
+		] 
+];
+
+echo Nav::widget ( [ 
+		'options' => [ 
+				'class' => 'navbar-nav navbar-right' 
+		],
+		'items' => $menuItems 
+] );
+NavBar::end ();
+?>
+    
+	<div class="container"></div>
+
+	<div class="wrap">
+
+
+		<div class="container">
+        <?=Breadcrumbs::widget ( [ 'links' => isset ( $this->params ['breadcrumbs'] ) ? $this->params ['breadcrumbs'] : [ ] ] )?>
+        <?= Alert::widget()?>
+        <?= $content?>
         </div>
-    </div>
+	</div>
 
-    <footer class="footer">
-        <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
-        </div>
-    </footer>
+	<div class="clearfix"></div>
+	<!--footer-->
+	<div class="clearfix"></div>
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-9">
+					<div class="footer_menus">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Store Locator</a></li>
+							<li><a href="#">My Rewards </a></li>
+							<li><a href="#">My List </a></li>
+							<li><a href="#">Mystore Offers </a></li>
+							<li><a href="#">GB Dictionary </a></li>
+							<li><a href="#">Legal Info </a></li>
+							<li><a href="#">Vendor Options </a></li>
+							<li><a href="#">Share App </a></li>
+						</ul>
+					</div>
+					<div class="footer_menus">
+						<ul>
+							<li><a href="#">Legal Info </a></li>
+							<li><a href="#"> Terms of use</a></li>
+							<li><a href="#">Privacy </a></li>
+							<li><a href="#">Infringement</a></li>
 
-    <?php $this->endBody() ?>
+						</ul>
+					</div>
+
+					<div class="side-bar">
+						<div class="follow">
+							<p>FOLLOW US:</p>
+							<ul>
+								<li><a href="#"><img src="images/fb.png"></a></li>
+								<li><a href="#"><img src="images/tiwitter.png"></a></li>
+								<li><a href="#"><img src="images/in.png"></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-3">
+					<div class="adress">
+						<h2>Address</h2>
+						<p>
+							#29-19-59, Dornakal Road, xxxxxxxxxxxxxxx,<br /> xxxxxxxxx.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+		<div class="bottom_footer">
+			<div class="container">
+				<p class="copy">Copyright &copy;<?= date('Y') ?> <?= Yii::$app->name; ?></p>
+				<p class="design">
+					Powered by: <a href="#" style="color: #888;">Expert Web Worx</a>
+				</p>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+
+    <?php $this->endBody()?>
+
+
+
+
 </body>
 </html>
-<?php $this->endPage() ?>
+<?php $this->endPage()?>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,51 +1,230 @@
 <?php
+use bupy7\flexslider\FlexSlider;
+use kartik\date\DatePicker;
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
 ?>
+
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+	<div class="body-content">
+		<div id="slider" class="flexslider">
+			<ul class="slides">
+			<?php
+			echo FlexSlider::widget ( [
+					'items' => ['<img src="images/slides/banner.jpg">',
+							['content' => '<img src="images/slides/banner1.jpg">',
+									'options' => ['class' => 'my-class']
+									]
+							]
+					] );
+			?>
+			</ul>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="left-sidebar">
+						<div class="shipping text-center">
+							<!--shipping-->
+							<img src="images/home/shipping1.jpg" alt="" />
+						</div>
+						<!--/shipping-->
+						<div class="shipping text-center">
+							<!--shipping-->
+							<img src="images/home/shipping.jpg" alt="" />
+						</div>
+						<!--/shipping-->
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+					</div>
+				</div>
+				<div class="col-sm-9 padding-right">
+					<div class="features_items">
+						<!--features_items-->
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<img src="images/home/product1.jpg" alt="" />
+										<h2>GROCERY STORE NAME</h2>
+										<p>
+											Sale Items <span class="no">25</span>
+										</p>
+										<a href="#" class="btn btn-default add-to-cart">Store Location</a>
+									</div>
+									<div class="product-overlay">
+										<div class="overlay-content">
+											<p>
+												Sale Items <span class="nm">25</span>
+											</p>
+											<a href="#" class="btn btn-default add-to-cart">Store
+												Location</a>
+										</div>
+									</div>
+									<p class="new">
+										10%<br /> Off
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<img src="images/home/product2.jpg" alt="" />
+										<h2>GROCERY STORE NAME</h2>
+										<p>
+											Sale Items <span class="no">50</span>
+										</p>
+										<a href="#" class="btn btn-default add-to-cart">Store Location</a>
+									</div>
+									<div class="product-overlay">
+										<div class="overlay-content">
+											<p>
+												Sale Items <span class="nm">50</span>
+											</p>
+											<a href="#" class="btn btn-default add-to-cart">Store
+												Location</a>
+										</div>
+									</div>
+									<p class="new">
+										10%<br /> Off
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<img src="images/home/product3.jpg" alt="" />
+										<h2>GROCERY STORE NAME</h2>
+										<p>
+											Sale Items <span class="no">05</span>
+										</p>
+										<a href="#" class="btn btn-default add-to-cart">Store Location</a>
+									</div>
+									<div class="product-overlay">
+										<div class="overlay-content">
+											<p>
+												Sale Items <span class="nm">05</span>
+											</p>
+											<a href="#" class="btn btn-default add-to-cart">Store
+												Location</a>
+										</div>
+									</div>
+									<p class="new">
+										10%<br /> Off
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<img src="images/home/product1.jpg" alt="" />
+										<h2>GROCERY STORE NAME</h2>
+										<p>
+											Sale Items <span class="no">25</span>
+										</p>
+										<a href="#" class="btn btn-default add-to-cart">Store Location</a>
+									</div>
+									<div class="product-overlay">
+										<div class="overlay-content">
+											<p>
+												Sale Items <span class="nm">25</span>
+											</p>
+											<a href="#" class="btn btn-default add-to-cart">Store
+												Location</a>
+										</div>
+									</div>
+									<p class="new">
+										10%<br /> Off
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<img src="images/home/product2.jpg" alt="" />
+										<h2>GROCERY STORE NAME</h2>
+										<p>
+											Sale Items <span class="no">50</span>
+										</p>
+										<a href="#" class="btn btn-default add-to-cart">Store Location</a>
+									</div>
+									<div class="product-overlay">
+										<div class="overlay-content">
+											<p>
+												Sale Items <span class="nm">50</span>
+											</p>
+											<a href="#" class="btn btn-default add-to-cart">Store
+												Location</a>
+										</div>
+									</div>
+									<p class="new">
+										10%<br /> Off
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<img src="images/home/product3.jpg" alt="" />
+										<h2>GROCERY STORE NAME</h2>
+										<p>
+											Sale Items <span class="no">05</span>
+										</p>
+										<a href="#" class="btn btn-default add-to-cart">Store Location</a>
+									</div>
+									<div class="product-overlay">
+										<div class="overlay-content">
+											<p>
+												Sale Items <span class="nm">25</span>
+											</p>
+											<a href="#" class="btn btn-default add-to-cart">Store
+												Location</a>
+										</div>
+									</div>
+									<p class="new">
+										10%<br /> Off
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--features_items-->
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
+<script>$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
+
+</script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/jquery.min.js" type="text/javascript"></script>
