6a82e4eca1d8b14409280694001f10b768841218
support <support@eww12.eww.local>
2015-08-25 11:22:05 +0530

    add vendor model

--- frontend/models/Vendorproducts.php
+++ frontend/models/Vendorproducts.php
@@ -3,16 +3,27 @@
 namespace app\models;
 
 use Yii;
+use yii\db\ActiveRecord;
 
 /**
  * This is the model class for table "vendorproducts".
  *
  * @property integer $Pid
+ * @property integer $Vpid
  * @property string $ProductName
- * @property string $Quatity
- * @property string $Weight
- * @property string $Price
- * @property string $Discount
+ * @property string $Brand
+ * @property string $Quantity
+ * @property string $UOM
+ * @property string $AltQty
+ * @property string $AltUom
+ * @property string $UnitPrice
+ * @property string $SalePrice
+ * @property string $PriceUnit
+ * @property string $StartDate
+ * @property string $EndsDate
+ * @property string $ReserveCount
+ * @property string $SaleInd
+ * @property string $FileUpload
  */
 class Vendorproducts extends \yii\db\ActiveRecord
 {
@@ -30,10 +41,12 @@ class Vendorproducts extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['ProductName', 'Quatity', 'Weight', 'Price', 'Discount'], 'required'],
-            [['ProductName'], 'string', 'max' => 200],
-            [['Quatity', 'Weight', 'Discount'], 'string', 'max' => 100],
-            [['Price'], 'string', 'max' => 10]
+            [['Vpid', 'ProductName', 'Brand', 'Quantity', 'UOM', 'AltQty', 'AltUom', 'UnitPrice', 'SalePrice', 'PriceUnit', 'StartDate', 'EndsDate', 'ReserveCount', 'SaleInd', 'FileUpload'], 'required'],
+            [['Vpid'], 'integer'],
+            [['ProductName', 'UOM', 'FileUpload'], 'string', 'max' => 200],
+            [['Brand', 'Quantity', 'AltQty', 'AltUom', 'UnitPrice', 'PriceUnit', 'SaleInd'], 'string', 'max' => 100],
+            [['SalePrice'], 'string', 'max' => 50],
+            [['StartDate', 'EndsDate', 'ReserveCount'], 'string', 'max' => 30]
         ];
     }
 
@@ -44,11 +57,46 @@ class Vendorproducts extends \yii\db\ActiveRecord
     {
         return [
             'Pid' => 'Pid',
-            'ProductName' => 'ProductName',
-            'Quatity' => 'Quatity',
-            'Weight' => 'Weight',
-            'Price' => 'Price',
-            'Discount' => 'Discount',
+            'Vpid' => 'Vpid',
+            'ProductName' => 'Product Name',
+            'Brand' => 'Brand',
+            'Quantity' => 'Quantity',
+            'UOM' => 'Uom',
+            'AltQty' => 'Alt Qty',
+            'AltUom' => 'Alt Uom',
+            'UnitPrice' => 'Unit Price',
+            'SalePrice' => 'Sale Price',
+            'PriceUnit' => 'Price Unit',
+            'StartDate' => 'Start Date',
+            'EndsDate' => 'Ends Date',
+            'ReserveCount' => 'Reserve Count',
+            'SaleInd' => 'Sale Ind',
+            'FileUpload' => 'File Upload',
         ];
     }
-}
+   
+   
+    	// Types
+    
+    	/**
+    	 * Selecting from the list of answers
+    	 */
+    	const TYPE_CLOSED = 1;
+    
+    	/**
+    	 * Writing answer manually
+    	 */
+    	const TYPE_OPENED = 2;
+    
+    	
+    	/**
+    	 * @return array
+    	*/
+  	public static function getTypesList()
+    	{
+    		return [
+    				self::TYPE_CLOSED => 'Closed',
+    				self::TYPE_OPENED => 'Opened',
+    		];
+    	}
+    }
